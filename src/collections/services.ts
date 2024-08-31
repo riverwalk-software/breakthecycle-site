69a2be7b3468105ca419ff9d6f62be5f1df0560c
wkenned1 <williamvkennedyy@gmail.com>
2024-08-31 18:57:56 +0000

    Add services

---
+++ src/collections/services.ts
@@ -0,0 +1,16 @@
+import { unique } from "@utils/unique";
+import { string } from "astro/zod";
+import { defineCollection, reference, z } from "astro:content";
+
+export default defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      benefits: z.array(z.string()),
+      image: image().optional(),
+      testimonials: z.string().optional(),
+      pricingOptions: z.array(z.string()),
+      longDesctipion: z.string(),
+      shortDescription: z.string(),
+    }),
+});
