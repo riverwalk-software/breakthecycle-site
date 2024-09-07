30b02a0ab883bda8305d5c67052eb465a9ac0fd4
wkenned1 <williamvkennedyy@gmail.com>
2024-09-07 18:18:16 +0000

    .
    Co-authored-by: Andrei Liviu Georgescu <andreilgeorgescu@users.noreply.github.com>

--- src/components/HeaderHelper.jsx
+++ src/components/HeaderHelper.jsx
@@ -4,23 +4,33 @@ import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import site from "../data/site.json";
+import { getCollection } from "astro:content";
 
-const navigation = [
-  { name: "Services", href: "/services" },
-  { name: "Events", href: "/events" },
-  { name: "About", href: "/about" },
-];
+let navigation = [];
+
+const services = await getCollection("events");
+const events = await getCollection("services");
+
+if (services && services.length > 0) {
+  navigation.push({ name: "Services", href: "/services" });
+}
+
+if (events && events.length > 0) {
+  navigation.push({ name: "Events", href: "/events" });
+}
+
+navigation.push({ name: "About", href: "/about" });
 
 export default function Example(props) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="shadow-slate-300 bg-white lg:bg-opacity-5 lg:bg-black">
+    <header className="shadow-slate-300 bg-white lg:bg-opacity-5 lg:bg-black overflow-hidden">
       <nav
         aria-label="Global"
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 overflow-hidden"
       >
-        <a href="/" className="-m-1.5 p-1.5">
+        <a href="/" className="-m-1.5 p-1.5 overflow-hidden">
           <span className="sr-only">{site.name}</span>
           {/* <img
             className="w-10"
@@ -48,12 +58,6 @@ export default function Example(props) {
               {item.name}
             </a>
           ))}
-          <a
-            href="https://sso.teachable.com/secure/256201/identity/login/password"
-            className="text-sm font-semibold leading-6 text-content"
-          >
-            Contact Us <span aria-hidden="true">&rarr;</span>
-          </a>
           {props.themeToggle}
         </div>
       </nav>
@@ -77,29 +81,6 @@ export default function Example(props) {
               <span className="sr-only">Close menu</span>
               <XMarkIcon aria-hidden="true" className="h-6 w-6" />
             </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-content hover:bg-gray-50"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-content hover:bg-gray-50"
-                >
-                  Log in
-                </a>
-              </div>
-            </div>
           </div>
         </DialogPanel>
       </Dialog>
